409117b1e510f7d871595e0b89c10038019f3d5b
reddymahendra52 <61696212+reddymahendra52@users.noreply.github.com>
2021-10-10 13:54:13 +0530

    Update cryptoEventApi.js

--- src/services/cryptoEventApi.js
+++ src/services/cryptoEventApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoEventHeaders = {
     'x-rapidapi-host': 'coingecko.p.rapidapi.com',
-    'x-rapidapi-key': '4143c0b6famsh890e23ac8fa634ap1e7241jsna5fdf97bd56d'
+    'x-rapidapi-key': <Use your API key>
 }
 
 const baseUrl = 'https://coingecko.p.rapidapi.com';
